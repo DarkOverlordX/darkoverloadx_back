ff0a1b63ae307ebfaa71455837dd3228e40a7e0b
CodeJesterND <dev.hyoseung@gmail.com>
2024-07-24 21:34:01 +0900

    ✅ Test: CI/CD 테스트 파일 추가

---
+++ src/main/java/darkoverload/itzip/sample/controller/MainController.java
@@ -0,0 +1,12 @@
+package darkoverload.itzip.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainP() {
+        return "main";
+    }
+}
