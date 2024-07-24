6117df1c6c1941442d2d5debc82085580a3c5c4a
CodeJesterND <dev.hyoseung@gmail.com>
2024-07-25 06:00:16 +0900

    ✅ Test: DB TEST File 추가

--- src/test/java/darkoverload/itzip/ItzipApplicationTests.java
+++ src/test/java/darkoverload/itzip/ItzipApplicationTests.java
@@ -1,13 +1,32 @@
 package darkoverload.itzip;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ItzipApplicationTests {
+@ActiveProfiles("local")
+public class ItzipApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    @DisplayName("DB 테스트 통과")
+    public void contextLoads() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection);
+            assertTrue(connection.isValid(1));
+        }
     }
-
 }
